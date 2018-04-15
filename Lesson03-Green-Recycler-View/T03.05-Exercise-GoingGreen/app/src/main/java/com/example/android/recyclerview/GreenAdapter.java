5398cd08f8d2e543d8e368c55818689948e6d2fe
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2018-04-15 12:42:29 +0300

    Android NanoDegree - Tutorial
    Android NanoDegree - Tutorial

--- Lesson03-Green-Recycler-View/T03.05-Exercise-GoingGreen/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.05-Exercise-GoingGreen/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -39,8 +39,8 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 
     private static final String TAG = GreenAdapter.class.getSimpleName();
 
-    // TODO (8) Add a private static int called viewHolderCount that will hold the total number of ViewHolders that are created
-
+    // TODO (COMPLETED) Add a private static int called viewHolderCount that will hold the total number of ViewHolders that are created
+    private static int viewHolderCount; // Oluşturulan toplam view holder sayısını tutacak değişken
     private int mNumberItems;
 
     /**
@@ -51,7 +51,9 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
      */
     public GreenAdapter(int numberOfItems) {
         mNumberItems = numberOfItems;
-        // TODO (9) When a new GreenAdapter is created, set the viewHolderCount to 0
+        // TODO (COMPLETED) When a new GreenAdapter is created, set the viewHolderCount to 0
+        // Yeni bir GreenAdapter oluşturulduğunda viewHolderCount değeri 0 ' a eşitleniyor.
+        viewHolderCount = 0;
     }
     /**
      *
@@ -75,12 +77,20 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
         View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
         NumberViewHolder viewHolder = new NumberViewHolder(view);
 
-        // TODO (12) Set the text of viewHolderIndex to "ViewHolder index: " + viewHolderCount
+        // TODO (COMPLETED) Set the text of viewHolderIndex to "ViewHolder index: " + viewHolderCount
+        // ViewHolderIndex ' in değerini atıyoruz.
+        viewHolder.viewHolderIndex.setText("ViewHolder index: " + viewHolderCount);
 
-        // TODO (13) Use ColorUtils.getViewHolderBackgroundColorFromInstance and pass in a Context and the viewHolderCount
-        // TODO (14) Set the background color of viewHolder.itemView with the color from above
+        // TODO (COMPLETED) Use ColorUtils.getViewHolderBackgroundColorFromInstance and pass in a Context and the viewHolderCount
+        int backgroundColorForViewHolder = ColorUtils
+                .getViewHolderBackgroundColorFromInstance(context, viewHolderCount);
 
-        // TODO (15) Increment viewHolderCount and log its value
+        // TODO (COMPLETED) Set the background color of viewHolder.itemView with the color from above
+        viewHolder.itemView.setBackgroundColor(backgroundColorForViewHolder);
+
+        // TODO (COMPLETED) Increment viewHolderCount and log its value
+
+        viewHolderCount++;
 
         return viewHolder;
     }
@@ -119,7 +129,8 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 
         // Will display the position in the list, ie 0 through getItemCount() - 1
         TextView listItemNumberView;
-        // TODO (10) Add a TextView variable to display the ViewHolder index
+        // TODO (COMPLETED) Add a TextView variable to display the ViewHolder index
+        TextView viewHolderIndex;
 
         /**
          * Constructor for our ViewHolder. Within this constructor, we get a reference to our
@@ -133,7 +144,8 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 
             listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
 
-            // TODO (11) Use itemView.findViewById to get a reference to tv_view_holder_instance
+            // TODO (COMPLETED) Use itemView.findViewById to get a reference to tv_view_holder_instance
+            viewHolderIndex = (TextView) itemView.findViewById(R.id.tv_view_holder_instance);
         }
 
         /**
--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,19 +71,42 @@ public class MainActivity extends AppCompatActivity {
         mNumbersList.setAdapter(mAdapter);
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called main.xml
-    // TODO (3) Add one item to the menu with an ID of action_refresh
-    // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
-    // TODO (5) Set the orderInCategory value to 1 to make sure this item is the first in the list
-    // TODO (6) Set app:showAsAction to ifRoom to display the menu item in the ActionBar if there is room
+    // TODO (COMPLETED) Create a menu resource in res/menu/ called main.xml
+    // TODO (COMPLETED) Add one item to the menu with an ID of action_refresh
+    // TODO (COMPLETED) Set the title of the menu item to "Refresh" using strings.xml
+    // TODO (COMPLETED) Set the orderInCategory value to 1 to make sure this item is the first in the list
+    // TODO (COMPLETED) Set app:showAsAction to ifRoom to display the menu item in the ActionBar if there is room
 
 
-    // TODO (7) Override onCreateOptionsMenu
-    // TODO (8) Use getMenuInflater().inflate to inflate the menu
-    // TODO (9) Return true to display this menu
+    // TODO (COMPLETED) Override onCreateOptionsMenu
+    // TODO (COMPLETED) Use getMenuInflater().inflate to inflate the menu
+    // TODO (COMPLETED) Return true to display this menu
 
-    // TODO (10) Override onOptionsItemSelected
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    // TODO (COMPLETED) Override onOptionsItemSelected
     // TODO (11) Within this method, get the ID from the MenuItem
     // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
     // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemsId = item.getItemId(); // İlgili id değerini alıyor.
+        switch (itemsId) {
+
+            case R.id.action_refresh:
+                mAdapter = new GreenAdapter(NUM_LIST_ITEMS); // Yeni bir  adapter oluştur. İçine NUM_LIST_ITEMS ' ı gönder.
+                mNumbersList.setAdapter(mAdapter); // Adapter 'ü RecyclerView ' e aktar.
+                return true; // Olumlu döndür.
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
